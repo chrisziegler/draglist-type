3f5687fa3f6303feae8a645a896d90e9dccd4c35
Chris Ziegler <chrisrziegler@gmail.com>
2022-06-18 23:58:02 -0700

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import React, { useState } from 'react'
-import './App.css'
 import InputField from './components/InputField'
+import { Todo } from './model'
+import './App.css'
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      const newTodo = {
+        id: Math.floor(Math.random() * 10000),
+        todo,
+        isDone: false,
+      }
+      // setTodos(prevState => prevState.concat(newTodo))
+      setTodos([...todos, newTodo])
+      setTodo('')
+    }
+  }
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
+      {todos.map(t => (
+        <li key={t.id}>{t.todo}</li>
+      ))}
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './InputField.module.css'
 
 interface Props {
   todo: string
   setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent) => void
 }
 
 // const InputField = ({ todo, setTodo }: Props) => {
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({
+  todo,
+  setTodo,
+  handleAdd,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
-    <form className={styles.input}>
+    <form
+      className={styles.input}
+      onSubmit={e => {
+        handleAdd(e)
+        inputRef.current?.blur()
+      }}>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Enter a task"
         className={styles.input__box}
+        value={todo}
+        onChange={event => setTodo(event.target.value)}
       />
       <button className={styles.input__submit}>Go</button>
     </form>
